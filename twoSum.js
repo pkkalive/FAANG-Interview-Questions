bf13df5f5523e335a77a8fb36e1dccdb7d644c92
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2021-02-18 20:18:18 +0530

    two sum solution 2

--- twoSum.js
+++ twoSum.js
@@ -7,7 +7,6 @@ const arr = [1, 3, 7, 9, 2];
 const target = 11;
 
 function twoSum(array, key) {
-  let result;
   if (array.length < 2) {
     return "Given Array is empty or it less than two entries";
   } else {
@@ -15,14 +14,42 @@ function twoSum(array, key) {
       for (let j = i + 1; j < array.length; j++) {
         if (array[i] + array[j] === key){
           return [i , j];
-        } else {
-          result = "No Match Found";
         }
       }
     }
-    return result;
+    return 
   }
   
 }
 
 console.log(twoSum(arr, target));
+
+// Solution 2
+const arr1 = [1, 3, 7, 9, 2];
+const target1 = 11;
+
+function twoSum2(array, key) {
+  let result = [];
+  if (array.length < 2) {
+    return "Given Array is empty or it less than two entries";
+  } else {
+    for (let i = 0; i < array.length; i++){
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] === key - array[i]){
+          result.push(i);
+          result.push(j);
+        }
+      }
+    }
+    if (result.length === 2){
+      return result
+    } else if(result.length > 2 ){
+      return "Given array has more than one possible solutions"
+    } else {
+      return "No Match Found";
+    }
+  }
+  
+}
+
+console.log(twoSum2(arr1, target1));
