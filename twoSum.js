272d89b0ce1669367bf158808916dc356e6b3045
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2021-02-18 20:02:36 +0530

    two sum solution 1

--- twoSum.js
+++ twoSum.js
@@ -3,7 +3,7 @@
 */
 
 // Solution 1
-const arr = [1, 3, 7, 9];
+const arr = [1, 3, 7, 9, 2];
 const target = 11;
 
 function twoSum(array, key) {
@@ -14,7 +14,7 @@ function twoSum(array, key) {
     for (let i = 0; i < array.length; i++){
       for (let j = i + 1; j < array.length; j++) {
         if (array[i] + array[j] === key){
-          return [i, j];
+          return [i , j];
         } else {
           result = "No Match Found";
         }
