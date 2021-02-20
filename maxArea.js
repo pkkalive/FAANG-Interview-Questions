4ec6d6d83781b8a9f7a02b98c2c3cd7e1b8d5637
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2021-02-21 00:48:05 +0530

    maxArea.js solution 3

--- maxArea.js
+++ maxArea.js
@@ -37,3 +37,22 @@ function findMaxArea2(heights) {
 }
 
 console.log(findMaxArea2(arr));
+
+// Solution 3
+function findMaxArea3(heights) {
+  let i = 0, j = heights.length - 1; maxArea = 0;
+  while (i < j) {
+    const height = Math.min(heights[i], heights[j]);
+    const width = (j - i);
+    const area = height * width;
+    maxArea = Math.max(maxArea, area);
+    if (heights[i] <= heights[j]){
+      i++;
+    } else {
+      j--;
+    }
+  }
+  return maxArea;
+}
+
+console.log(findMaxArea3(arr));
