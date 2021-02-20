99f2d12a4ae4ab75d03097291d7b8a05cbb4d154
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2021-02-20 18:18:55 +0530

    twoSuum.js solution 3

--- twoSum.js
+++ twoSum.js
@@ -7,49 +7,58 @@ const arr = [1, 3, 7, 9, 2];
 const target = 11;
 
 function twoSum(array, key) {
-  if (array.length < 2) {
-    return "Given Array is empty or it less than two entries";
-  } else {
-    for (let i = 0; i < array.length; i++){
-      for (let j = i + 1; j < array.length; j++) {
-        if (array[i] + array[j] === key){
-          return [i , j];
-        }
+  for (let i = 0; i < array.length; i++){
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === key){
+        return [i , j];
       }
     }
-    return 
   }
-  
+  return "No Match Found";
 }
 
 console.log(twoSum(arr, target));
 
 // Solution 2
-const arr1 = [1, 3, 7, 9, 2];
-const target1 = 11;
-
 function twoSum2(array, key) {
   let result = [];
+  for (let i = 0; i < array.length; i++){
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === (key - array[i])){
+        result.push(i);
+        result.push(j);
+      }
+    }
+  }
+  if (result.length === 2){
+    return result
+  } else if(result.length > 2 ){
+    return "Given array has more than one possible solutions"
+  } else {
+    return "No Match Found";
+  }
+}
+
+console.log(twoSum2(arr, target));
+
+// Solution 3
+function twoSum3(array, key) {
+  let result = {}, j = 1
   if (array.length < 2) {
     return "Given Array is empty or it less than two entries";
   } else {
     for (let i = 0; i < array.length; i++){
-      for (let j = i + 1; j < array.length; j++) {
-        if (array[j] === key - array[i]){
-          result.push(i);
-          result.push(j);
-        }
+      if (!result[key - array[i]]){
+        result[key - array[i]] = i
       }
     }
-    if (result.length === 2){
-      return result
-    } else if(result.length > 2 ){
-      return "Given array has more than one possible solutions"
-    } else {
-      return "No Match Found";
+    for (let i = 0; i < array.length; i++){
+      if (result[array[i]]){
+        return [i, result[array[i]]]
+      }
     }
   }
   
 }
 
-console.log(twoSum2(arr1, target1));
+console.log(twoSum3(arr, target));
