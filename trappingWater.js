8b5e0a335e15bc7a9450f749fae1960b89dd9bea
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2021-02-21 12:26:59 +0530

    trappedWater.js Solution 1

---
+++ trappingWater.js
@@ -0,0 +1,26 @@
+/*
+  Given an array of integers representing am elevation map where the width of each bar is 1, return how much rainwater can be trapped 
+*/
+
+//Solution 1
+const arr = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2];
+
+function trappedWater(heights) {
+  let totalWater = 0;
+  for (let i = 0; i < heights.length; i++){
+    let left = i, right = i, maxLeft = 0, maxRight = 0;
+    while (left >= 0){
+      maxLeft = Math.max(maxLeft, heights[left]);
+      left --;
+    }
+    while (right < heights.length ){
+      maxRight = Math.max(maxRight, heights[right]);
+      right ++;
+    }
+    const currentWater = Math.min(maxLeft, maxRight) - heights[i];
+    totalWater += currentWater;
+  }
+  return totalWater;
+}
+
+console.log(trappedWater(arr));
