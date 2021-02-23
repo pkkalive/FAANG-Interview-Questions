f916bc91e27187e70052d259d467291f13f0c01d
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2021-02-23 12:26:06 +0530

    typedOutStrings.js Solution 2

--- typedOutStrings.js
+++ typedOutStrings.js
@@ -42,3 +42,35 @@ function typedOutString(S, T) {
 }
 
 console.log(typedOutString(S, T));
+
+//Solution 2
+
+function typedOutString2(S, T) {
+  const buildString = function(string) {
+    const builtString = [];
+    for(let p = 0; p < string.length; p++) {
+      if(string[p] !== '#') {
+        builtString.push(string[p]);
+      } else {
+        builtString.pop();
+      }
+    }  
+    return builtString;
+  }
+
+  const finalS = buildString(S);
+  const finalT = buildString(T);
+  
+  if(finalS.length !== finalT.length) {
+    return false;
+  } else {
+    for(let p = 0; p < finalS.length; p++) {
+      if(finalS[p] !== finalT[p]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+console.log(typedOutString2(S, T));
