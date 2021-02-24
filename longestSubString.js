60c0cf4491ec6da67a6ec9db4f49bbbf7ef7f290
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2021-02-24 19:03:01 +0530

    longestSubString.js Solution 3

--- longestSubString.js
+++ longestSubString.js
@@ -43,3 +43,22 @@ function longestSubString2(s) {
 }
 
 console.log(longestSubString2(str));
+
+// Solution 3
+function longestSubString3(s) {
+  if(s.length <= 1) return s.length;
+  const seen = new Map();
+  let left = 0, longest = 0;
+  for(let right = 0; right < s.length; right++) {
+    const currentChar = s[right];
+    const previouslySeenChar = seen.get(currentChar);
+    if(previouslySeenChar >= left) {
+      left = previouslySeenChar + 1;
+    }
+    seen.set(currentChar, right);
+    longest = Math.max(longest, right - left + 1);
+  }  
+  return longest;
+}
+
+console.log(longestSubString3(str));
