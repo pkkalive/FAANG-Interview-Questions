db06cdb117bff76590e73db2450567ea33ad3a30
Purushotham Kumar <purushothamkumar.kalive@hitachiconsulting.com>
2021-02-28 23:22:15 +0530

    reverseInteger.js Solution 2

--- reverseInteger.js
+++ reverseInteger.js
@@ -15,3 +15,16 @@ function reverseInteger(x) {
 }
 
 console.log(reverseInteger(123456));
+
+function reverseInteger2(x) {
+  let revNum = 0, lastDigit = 0;
+  while (x!==0) {
+      lastDigit = x % 10; 
+      x = parseInt(x/10); 
+      revNum = revNum * 10 + lastDigit;
+      if (revNum < Math.pow(-2, 31) || revNum > Math.pow(2, 31) - 1) return 0
+  }
+  return revNum
+}
+
+console.log(reverseInteger2(123456));
