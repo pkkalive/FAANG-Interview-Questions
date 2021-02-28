c25985edd96d84d98323f73cb153f3396b5bb6ad
Purushotham Kumar <purushothamkumar.kalive@hitachiconsulting.com>
2021-02-28 23:25:57 +0530

    reverseInteger.js Solution 4

--- reverseInteger.js
+++ reverseInteger.js
@@ -38,3 +38,22 @@ function reverseInteger3(x) {
 }
 
 console.log(reverseInteger3(123456));
+
+// Solution 4
+function reverseInteger4(x) {
+  const isNegative = x< 0 ? true : false;
+  if (isNegative){
+    x = x *-1;
+  }
+  let reversed = 0;
+  while(x>0){
+    reversed = (reversed * 10) + (x % 10);
+    x = parseInt(x/10);
+  }
+  if(reversed > 2**31){
+    return 0;
+  }
+  return isNegative? reversed * -1 : reversed;
+}
+
+console.log(reverseInteger4(123456));
